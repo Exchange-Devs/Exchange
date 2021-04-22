0df72759dc189ac93b9085b7ef00a573cce63c45
Kail Jean Pierre <spakimimi@gmail.com>
2021-04-22 16:31:36 -0400

    toast gone

--- app/src/main/java/com/example/exchange/MainActivity.java
+++ app/src/main/java/com/example/exchange/MainActivity.java
@@ -1,15 +1,14 @@
 package com.example.exchange;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
@@ -45,20 +44,16 @@ public class MainActivity extends AppCompatActivity
                 Fragment fragment;
                 switch (menuItem.getItemId()) {
                     case R.id.action_home:
-                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
                         fragment = new ListingsFragment();
                         break;
                     case R.id.action_message:
-                        Toast.makeText(MainActivity.this, "Messages", Toast.LENGTH_SHORT).show();
                         fragment = new ListingsFragment();
                         break;
                     case R.id.action_search:
-                        Toast.makeText(MainActivity.this, "Search", Toast.LENGTH_SHORT).show();
                         fragment = new ListingsFragment();
                         break;
                     case R.id.action_profile:
                     default:
-                        Toast.makeText(MainActivity.this, "My Profile", Toast.LENGTH_SHORT).show();
                         fragment = new MyProfileFragment();
                         break;
                 }
