c097cd02b95793bb2b5253363e89fa008e7aaef1
Kail Jean Pierre <spakimimi@gmail.com>
2021-05-03 23:02:55 -0400

    map

--- app/src/main/java/com/example/exchange/fragments/MapFragment.java
+++ app/src/main/java/com/example/exchange/fragments/MapFragment.java
@@ -32,10 +32,12 @@ public class MapFragment extends Fragment {
                 getChildFragmentManager().findFragmentById(R.id.google_map);
 
         // Async map
-        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+        supportMapFragment.getMapAsync(new OnMapReadyCallback(){
+
             @Override
             public void onMapReady(GoogleMap googleMap) {
                 googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
                     @Override
                     public void onMapClick(LatLng latLng) {
                         // by clicking on the map
@@ -54,8 +56,6 @@ public class MapFragment extends Fragment {
                         googleMap.addMarker(markerOptions);
                     }
                 });
-
-
             }
         });
 
